3d887581150106ac28e9b1f35aaa7edd9e056c33
Yadeni Getu <yadeni.ugr-7202-16@aau.edu.et>
2026-05-29 00:26:46 +0300

    Fix flake8 E501 line length issue

--- src/train.py
+++ src/train.py
@@ -4,7 +4,13 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score
+)
 
 from src.data_processing import preprocess_data, build_rfm_target
 
