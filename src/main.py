5c78530c1fa8031e454a64a9be9acfa1fdfe23b6
Yadeni Getu <yadeni.ugr-7202-16@aau.edu.et>
2026-05-29 10:24:41 +0300

    Fix final flake8 E501 issues

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 import numpy as np
 import joblib
 
-from src.api.pydantic_models import PredictionRequest, PredictionResponse
+from src.api.pydantic_models import (
+    PredictionRequest,
+    PredictionResponse
+)
 
 app = FastAPI(title="Credit Risk API")
 
@@ -17,9 +20,16 @@ def home():
 @app.post("/predict", response_model=PredictionResponse)
 def predict(data: PredictionRequest):
 
-    X = np.array([[data.total_amount, data.avg_amount, data.transaction_count]])
+    X = np.array([
+        [
+            data.total_amount,
+            data.avg_amount,
+            data.transaction_count
+        ]
+    ])
 
     prob = model.predict_proba(X)[0][1]
+
     pred = int(prob > 0.5)
 
     return PredictionResponse(
