73a404a44e2838246e536916a1f22e8a6ca257b8
Yadeni Getu <yadeni.ugr-7202-16@aau.edu.et>
2026-07-27 12:36:44 +0300

    Add conftest.py to guarantee src imports for pytest in any environment

--- conftest.py
+++ conftest.py
@@ -1,6 +1,4 @@
 import sys
 from pathlib import Path
 
-# Force the project root directory into sys.path
-root_dir = Path(__file__).resolve().parent
-sys.path.insert(0, str(root_dir))
+sys.path.insert(0, str(Path(__file__).resolve().parent))
